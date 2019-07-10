9b92a68d43f768320f8280a362808f83d5b1d0a5
Sahapat <sahapat.starcaft@gmail.com>
2019-07-10 22:09:53 +0700

    Fix description and add Ex input output for testing an algorithm

--- Exercise3.cs
+++ Exercise3.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections;
 
 using static System.Console;
 
 namespace Exercise
 {
+    /* 
+    At the village, have a houses align as horizontal line with an assign's number 1 to N. You're the mayer of village want to paint the house with lowest price and condition, Can't have save color more than 2 closed house.
+
+    Ex Input          |        Output
+       3 2
+       1 1 10                  15
+       1 2 4
+       2 1 7
+       2 2 4
+       3 1 8
+       3 2 4
+
+    Select
+       House 1 Color 2 Price 4
+       House 2 Color 1 Price 7
+       House 3 Color 2 Price 4
+
+       Sum price >> 15 Lowest price
+     */
     public class ColorfulVillageProblem
     {
         public static void Run()
